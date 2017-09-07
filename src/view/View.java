2fef2b7f0e06edfc55b6ba4217414f0a92b0d098
TomaszSowa <tsowa974@gmail.com>
2017-09-07 20:00:45 +0200

    Update 'View' class.

--- src/view/View.java
+++ src/view/View.java
@@ -3,18 +3,17 @@ package view;
 
 public class View {
 
-    private String ADMIN = "1) create Mentor account\n"
+    final private String ADMIN = "1) create Mentor account\n"
             + "2) create Class\n3) edit Mentor's profile\n"
             + "4) see Mentor's profile\n5) create Level";
 
-    private String MENTOR = "1) create Codecooler account\n2) Add Quest\n"
+    final private String MENTOR = "1) create Codecooler account\n2) Add Quest\n"
             + "3) split Quests\n4) add Artifact\n5) update Quest/Artifact\n"
             + "6) split Artifacts\n7) mark Achieved Quest\n"
             + "8) mark Bought Artifact\n9) see codecoller's Wallet";
 
-    private String STUDENT = "1) see Wallet\n2) buy Artifact\n"
+    final private String STUDENT = "1) see Wallet\n2) buy Artifact\n"
             + "3) group buying\n4) see Level";
-
 
     public void showMenu(String owner) {
         String menu = "\nChoose option:\n0) exit menu\n";
@@ -24,8 +23,20 @@ public class View {
         else if (owner.equals("student")) menu += STUDENT;
 
         System.out.println(menu);
-
     }
+
+    public void getString() {
+        Sy
+        String login = getString();
+       
+    }
+
+    public void getLogin() {
+        String password = getString();
+       
+    }
+    
+
 
 
 }
