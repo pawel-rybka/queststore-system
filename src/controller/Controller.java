a3d369f29ff81900ed99fd3b57bde19580399a46
Pawel Rybka <pwl.rybka@gmail.com>
2017-09-08 10:02:31 +0200

    Add file with class Controller

---
+++ src/controller/Controller.java
@@ -0,0 +1,7 @@
+package controller;
+
+public class Controller {
+    // private AdminController adminController = new AdminController();
+    private MentorController mentorController = new MentorController();
+    // private StudentController 
+}
