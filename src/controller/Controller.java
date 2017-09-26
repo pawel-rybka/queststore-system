b95382d468e25d05411fc707c4632cf91793f9af
backtake <stasiusoltysik@gmail.com>
2017-09-26 11:01:03 +0200

    reverse changes

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,7 +1,10 @@
 package controller;
 
+import java.util.Scanner;
+
 public class Controller {
     // private AdminController adminController = new AdminController();
     private MentorController mentorController = new MentorController();
-    // private StudentController 
+    // private StudentController
+
 }
