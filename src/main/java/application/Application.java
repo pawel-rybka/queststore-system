8ccf443fd49f148ab320114e718bfd7191561f18
backtake <stasiusoltysik@gmail.com>
2017-09-28 11:17:16 +0200

    Merging files

--- src/main/java/application/Application.java
+++ src/main/java/application/Application.java
@@ -2,17 +2,14 @@ package application;
 
 import java.util.Iterator;
 import java.util.Scanner;
-import model.Mentor;
-import model.Student;
+
 import model.User;
-import dao.AbstractDao;
 import dao.MentorDao;
 import view.View;
 
 public class Application {
 
     private User loggedUser;
-    private AbstractDao dao;
     private Scanner stdin;
     private View view;
 
@@ -29,9 +26,7 @@ public class Application {
 
     public void run() {
 
-        this.dao = new MentorDao();
-        ((MentorDao)dao).createObjectsFromList();
-        this.loggedUser = login();
+//        this.loggedUser = login();
 
         if (loggedUser != null) {
             this.view.printMenu("mentor");
@@ -39,20 +34,19 @@ public class Application {
         stdin.close();
     }
 
-    private User login() {
-        String userEmail = this.view.getUserLogin();
-        String userPassword = this.view.getUserPassword();
-        Iterator iterator = this.dao.getIterator();
-        User user = null;
-        User foundUser = null;
-
-        while (iterator.hasNext()) {
-            user = (User)iterator.next();
-            if (user.getEmail().equals(userEmail) && user.getPassword().equals(userPassword)) {
-                foundUser = user;
-            }
-        }
-
-        return foundUser;
+    private void login() {
+//        String userEmail = this.view.getUserLogin();
+//        String userPassword = this.view.getUserPassword();
+//        User user = null;
+//        User foundUser = null;
+//
+//        while (iterator.hasNext()) {
+//            user = (User)iterator.next();
+//            if (user.getEmail().equals(userEmail) && user.getPassword().equals(userPassword)) {
+//                foundUser = user;
+//            }
+//        }
+//
+//        return foundUser;
     }
 }
--- src/main/java/controller/Main.java
+++ src/main/java/controller/Main.java
@@ -3,13 +3,13 @@ package controller;
 public class Main {
 
     public static void main(String[] args) {
-        Main main = new Main();
-        main.test();
+//        Main main = new Main();
+//        main.test();
     }
 
-    private void test() {
-       
-        MentorController mc = new MentorController(new View(stdin));
-        mc.handleMenu();
-    }
+//    private void test() {
+//
+//        MentorController mc = new MentorController(new View(stdin));
+//        mc.handleMenu();
+//    }
 }
