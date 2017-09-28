d966831dd1d8a4c3ad2679c7d3a671db4a660ad8
backtake <stasiusoltysik@gmail.com>
2017-09-28 13:26:23 +0200

    Implement class Artifact with constructors and getters

--- src/main/java/model/Artifact.java
+++ src/main/java/model/Artifact.java
@@ -4,5 +4,29 @@ public class Artifact {
 
     Integer id;
     String category;
+    Integer price;
 
+    public Artifact(Integer id, String category, Integer price) {
+        this.id = id;
+        this.category = category;
+        this.price = price;
+    }
+
+    public Artifact(String category, Integer price) {
+        this.id = null;
+        this.category = category;
+        this.price = price;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
 }
