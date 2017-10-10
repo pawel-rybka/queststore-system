c0d4c5ad8c457255b64a93e3ca4c1312f4a31d0b
Pawel Rybka <pwl.rybka@gmail.com>
2017-10-10 11:56:02 +0200

    Add getMentors, addObject, updateData methods in MentorDao class

--- src/main/java/dao/MentorDao.java
+++ src/main/java/dao/MentorDao.java
@@ -7,19 +7,45 @@ import java.util.ArrayList;
 
 public class MentorDao extends AbstractDao<Mentor> {
 
-    private Connection c = null;
-    private Statement stmt = null;
+    private Connection conn = null;
 
     public MentorDao(){
-        c = DBConnection.getC();
+        conn = DBConnection.getC();
     }
 
-    public ArrayList<Mentor> createObjectFromDatabase() throws SQLException {
-        ArrayList<Mentor> mentors = new ArrayList<Mentor>();
-        stmt = c.createStatement();
-        ResultSet rs = stmt.executeQuery( "SELECT * FROM Mentors;" );
+    public ArrayList<Mentor> getMentors() throws SQLException {
+        ArrayList<Mentor> mentors = new ArrayList<>();
+        String sql = "SELECT * FROM Mentors;";
+
+        Statement stmt = conn.createStatement();
+        ResultSet rs = stmt.executeQuery(sql);
 
         while ( rs.next() ) {
+            Integer id = rs.getInt("id");
+            String firstName = rs.getString("first_name");
+            String lastName = rs.getString("last_name");
+            String phoneNumber = rs.getString("phone_number");
+            String email = rs.getString("email");
+            String password = rs.getString("password");
+
+            Mentor newMentor = new Mentor(id, firstName, lastName, phoneNumber, email, password);
+            mentors.add(newMentor);
+        }
+        stmt.close();
+        return mentors;
+    }
+
+    public Mentor createUserObject(String inputEmail, String inputPassword) throws SQLException {
+        Mentor newMentor = null;
+        String sql = "SELECT * FROM Mentors WHERE email = ? AND password = ?;";
+
+        PreparedStatement pstmt = conn.prepareStatement(sql);
+        pstmt.setString(1, inputEmail);
+        pstmt.setString(2, inputPassword);
+
+        ResultSet rs = pstmt.executeQuery();
+
+        if (rs.next()) {
             int id = rs.getInt("id");
             String firstName = rs.getString("first_name");
             String lastName = rs.getString("last_name");
@@ -27,35 +53,43 @@ public class MentorDao extends AbstractDao<Mentor> {
             String email = rs.getString("email");
             String password = rs.getString("password");
 
-            Mentor newMentor = new Mentor(id, firstName, lastName,
-                    phoneNumber, email, password);
-            mentors.add(newMentor);
+            newMentor = new Mentor(id, firstName, lastName, phoneNumber, email, password);
         }
-        stmt.close();
-        return mentors;
+
+        pstmt.close();
+        rs.close();
+        return newMentor;
     }
 
-    public Mentor createUserObject(String inputEmail, String inputPassword) throws SQLException{
+    public void addObject(Mentor mentor) throws SQLException {
+        String sql = "INSERT INTO Mentors (first_name, last_name, phone_number, email, password)" +
+                "VALUES (?, ?, ?, ?, ?);";
 
-        String sql = String.format("SELECT * FROM Mentors WHERE email = ? AND password = ?;");
+        PreparedStatement pstmt = conn.prepareStatement(sql);
+        pstmt.setString(1, mentor.getFirstName());
+        pstmt.setString(2, mentor.getLastName());
+        pstmt.setString(3, mentor.getPhoneNumber());
+        pstmt.setString(4, mentor.getEmail());
+        pstmt.setString(5, mentor.getPassword());
+        pstmt.executeUpdate();
 
-        PreparedStatement pstmt = c.prepareStatement(sql);
-        pstmt.setString(1, inputEmail);
-        pstmt.setString(2, inputPassword);
+        pstmt.close();
+    }
 
-        ResultSet rs = pstmt.executeQuery();
+    public void updateData(Mentor mentor) throws SQLException {
+        String sql = "UPDATE Mentors SET first_name = ?, last_name = ?, phone_number = ?, "
+                     +"email = ?, password = ? WHERE id = ?;";
 
-        int id = rs.getInt("id");
-        String firstName = rs.getString("first_name");
-        String lastName = rs.getString("last_name");
-        String phoneNumber = rs.getString("phone_number");
-        String email = rs.getString("email");
-        String password = rs.getString("password");
+        try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setString(1, mentor.getFirstName());
+            pstmt.setString(2, mentor.getLastName());
+            pstmt.setString(3, mentor.getPhoneNumber());
+            pstmt.setString(4, mentor.getEmail());
+            pstmt.setString(5, mentor.getPassword());
+            pstmt.setInt(4, mentor.getId());
 
-        Mentor newMentor = new Mentor(id, firstName, lastName,
-                phoneNumber, email, password);
-
-        return newMentor;
+            pstmt.executeUpdate();
+        }
     }
 }
 
