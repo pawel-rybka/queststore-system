a224a9bcab189c47fd75431a770a0c12669c4936
Pawel Rybka <pwl.rybka@gmail.com>
2017-10-10 13:50:47 +0200

    Add try-with-resources statement in addObject method in MentorDao class

--- src/main/java/dao/MentorDao.java
+++ src/main/java/dao/MentorDao.java
@@ -65,16 +65,17 @@ public class MentorDao extends AbstractDao<Mentor> {
         String sql = "INSERT INTO Mentors (first_name, last_name, phone_number, email, password)" +
                 "VALUES (?, ?, ?, ?, ?);";
 
-        PreparedStatement pstmt = conn.prepareStatement(sql);
-        pstmt.setString(1, mentor.getFirstName());
-        pstmt.setString(2, mentor.getLastName());
-        pstmt.setString(3, mentor.getPhoneNumber());
-        pstmt.setString(4, mentor.getEmail());
-        pstmt.setString(5, mentor.getPassword());
-        pstmt.executeUpdate();
-
-        pstmt.close();
+        try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setString(1, mentor.getFirstName());
+            pstmt.setString(2, mentor.getLastName());
+            pstmt.setString(3, mentor.getPhoneNumber());
+            pstmt.setString(4, mentor.getEmail());
+            pstmt.setString(5, mentor.getPassword());
+            pstmt.executeUpdate();
+        }
     }
+
+
 
     public void updateData(Mentor mentor) throws SQLException {
         String sql = "UPDATE Mentors SET first_name = ?, last_name = ?, phone_number = ?, "
@@ -86,8 +87,7 @@ public class MentorDao extends AbstractDao<Mentor> {
             pstmt.setString(3, mentor.getPhoneNumber());
             pstmt.setString(4, mentor.getEmail());
             pstmt.setString(5, mentor.getPassword());
-            pstmt.setInt(4, mentor.getId());
-
+            pstmt.setInt(6, mentor.getId());
             pstmt.executeUpdate();
         }
     }
