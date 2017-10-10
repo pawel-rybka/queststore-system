dc3f9266527d84177fae2f9556bf0ebc9d8420e2
polbox93 <polbox93@gmail.com>
2017-10-10 10:03:12 +0200

    Delete some usless code lines (Scanner object and functions)

--- src/main/java/view/ControllerView.java
+++ src/main/java/view/ControllerView.java
@@ -1,20 +1,16 @@
 package view;
 
-import java.util.Scanner;
 
 public class ControllerView extends View {
-
-    private Scanner csan;
-
-    public ControllerView() {
-        Scanner csan = new Scanner(System.in);
-    }
 
     final private String MENU = "\nLogin as: " +
                                 "\n1) Admin" +
                                 "\n2) Mentor" +
                                 "\n3) Student" +
                                 "\n0) Quit";
+
+    public ControllerView() {
+    }
 
 
     public void printMenu() {
