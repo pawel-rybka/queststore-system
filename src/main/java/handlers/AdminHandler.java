185e3f737f64e56a817d57da725e995c30fc82ac
backtake <stasiusoltysik@gmail.com>
2017-11-08 16:00:46 +0100

    Add functionality to print details of certain mentor

--- src/main/java/handlers/AdminHandler.java
+++ src/main/java/handlers/AdminHandler.java
@@ -3,9 +3,16 @@ package handlers;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.URI;
+import java.net.URLDecoder;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import dao.MentorDao;
+import model.Mentor;
 import org.jtwig.JtwigModel;
 import org.jtwig.JtwigTemplate;
 
@@ -41,12 +48,35 @@ public class AdminHandler implements HttpHandler {
 
         }else if (path.equals("/admin/see-mentor")) {
             if (method.equals("GET")) {
-                template = JtwigTemplate.classpathTemplate("static/admin/see-mentor.html");
+                template = JtwigTemplate.classpathTemplate("templates/see-mentor.twig");
                 model  = JtwigModel.newModel();
+                MentorDao mDao = new MentorDao();
+                ArrayList<Mentor> mentors = null;
+                try {
+                    mentors = mDao.getMentors();
+                    model.with("mentors", mentors);
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
 
             } else if (method.equals("POST")) {
-                template = JtwigTemplate.classpathTemplate("static/admin/see-mentor-2.html");
+
+                InputStreamReader isr = new InputStreamReader(httpExchange.getRequestBody(), "UTF-8");
+                BufferedReader br = new BufferedReader(isr);
+                String formData = br.readLine();
+
+                Map inputs = parseFormData(formData);
+                MentorDao mDao = new MentorDao();
+                template = JtwigTemplate.classpathTemplate("templates/see-mentor-2.twig");
                 model  = JtwigModel.newModel();
+
+                try {
+                    Mentor mentor = mDao.getMentorById(Integer.valueOf(inputs.get("mentor").toString()));
+                    model.with("mentor", mentor);
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+
             }
 
         }else if (path.equals("/admin/edit-mentor")) {
@@ -85,5 +115,20 @@ public class AdminHandler implements HttpHandler {
         os.write(response.getBytes());
         os.close();
 
+    }
+
+    private static Map<String, String> parseFormData(String formData) throws UnsupportedEncodingException {
+        Map<String, String> map = new HashMap<>();
+        String[] pairs = formData.split("&");
+        for(String pair : pairs){
+            String[] keyValue = pair.split("=");
+            if (keyValue.length == 1) {
+                map.put(keyValue[0], "");
+            } else {
+                String value = URLDecoder.decode(keyValue[1], "UTF-8");
+                map.put(keyValue[0], value);
+            }
+        }
+        return map;
     }
 }
--- src/main/java/handlers/App.java
+++ src/main/java/handlers/App.java
@@ -8,7 +8,7 @@ public class App {
 
 
     public static void main(String[] args) throws Exception {
-        HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+        HttpServer server = HttpServer.create(new InetSocketAddress(8010), 0);
 
         server.createContext("/admin", new AdminHandler());
         server.createContext("/static", new Static());
