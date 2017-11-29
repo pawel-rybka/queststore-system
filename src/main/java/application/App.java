54358ea90555cefeb5af5ae48258e21dba3416f4
Pawel Rybka <pwl.rybka@gmail.com>
2017-11-29 11:01:39 +0100

    Add context for logout and pass sessionsData map to handlers

--- src/main/java/application/App.java
+++ src/main/java/application/App.java
@@ -2,23 +2,29 @@ package application;
 
 import com.sun.net.httpserver.HttpServer;
 import handlers.*;
+import model.User;
 
 import java.net.InetSocketAddress;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 public class App {
 
 
     public static void main(String[] args) throws Exception {
+        Map<UUID, User> sessionsData = new HashMap<>();
         HttpServer server = HttpServer.create(new InetSocketAddress(8010), 0);
 
-        server.createContext("/admin", new AdminHandler());
+        server.createContext("/login", new LoginHandler(sessionsData));
+        server.createContext("/logout", new LogoutHandler(sessionsData));
+        server.createContext("/admin", new AdminHandler(sessionsData));
         server.createContext("/static", new Static());
-        server.createContext("/login", new LoginHandler());
-        server.createContext("/mentor", new MentorHandler());
-        server.createContext("/student", new StudentHandler());
+        server.createContext("/mentor", new MentorHandler(sessionsData));
+        server.createContext("/student", new StudentHandler(sessionsData));
         server.setExecutor(null);
 
         server.start();
-        }
+    }
 
 }
